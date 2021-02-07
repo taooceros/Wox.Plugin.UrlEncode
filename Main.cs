45e632c316d2d82f60396253b424a0379a82903b
张弘韬 <hongtao_zhang@outlook.com>
2021-02-07 14:53:18 +0800

    Migrate to Flow

--- Main.cs
+++ Main.cs
@@ -3,65 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web;
-using System.Windows.Forms;
+using System.Windows;
+using Flow.Launcher.Plugin;
 
 namespace Wox.Plugin.UrlEncode
 {
     public class Main : IPlugin
     {
-        
-
         public List<Result> Query(Query query)
         {
             var list = new List<Result>();
-            if (query.ActionParameters.Count == 0)
+            if (query.Search.Length == 0)
             {
-                list.Add(new Result() {
-                  IcoPath ="Images\\app.png",
-                   Title = "Please input a string"
+                list.Add(new Result()
+                {
+                    IcoPath = "Images\\app.png",
+                    Title = "Please input a string"
                 });
                 return list;
             }
 
-            if (query.ActionParameters.Count > 0)
+            var str = query.Search;
+            list.Add(new Result()
             {
-                var str = query.ActionParameters[0];
-                list.Add(new Result()
+                IcoPath = "Images\\encode.png",
+                Title = HttpUtility.UrlEncode(str),
+                SubTitle = "Copy to clipboard",
+                Action = (c) =>
                 {
-                    IcoPath = "Images\\encode.png",
-                    Title = HttpUtility.UrlEncode(str),
-                    SubTitle = "Copy to clipboard",
-                    Action = (c) =>
-                    {
-                        Clipboard.SetText(HttpUtility.UrlEncode(str));
-                        return true;
-                    }
-                });
-                list.Add(new Result()
-                {
-                    IcoPath = "Images\\decode.png",
-                    Title = HttpUtility.UrlDecode(str),
-                    SubTitle = "Copy to clipboard",
-                    Action = (c) =>
-                    {
-                        Clipboard.SetText(HttpUtility.UrlDecode(str));
-                        return true;
-                    }
-                });
-
-                if (str.Contains("%"))
-                {
-                    // may be a encode string, change order
-                    list.Reverse();
+                    Clipboard.SetDataObject(HttpUtility.UrlEncode(str));
+                    return true;
                 }
+            });
+            list.Add(new Result()
+            {
+                IcoPath = "Images\\decode.png",
+                Title = HttpUtility.UrlDecode(str),
+                SubTitle = "Copy to clipboard",
+                Action = (c) =>
+                {
+                    Clipboard.SetDataObject(HttpUtility.UrlDecode(str));
+                    return true;
+                }
+            });
+
+            if (str.Contains("%"))
+            {
+                // may be a encode string, change order
+                list.Reverse();
             }
+
 
             return list;
         }
 
         public void Init(PluginInitContext context)
         {
-
         }
     }
 }
